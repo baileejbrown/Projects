eb57ea5d222e3b6abe5e1d90056d74c3da79fd21
Bailee <bailee.brown@my.jcu.edu.au>
2019-08-29 11:30:33 +1000

    add list_exercises

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,23 @@
+"""print('Please enter 5 numbers!')
+numbers = []
+
+for i in range(0,5):
+    number = int(input('Number {}: '.format(i+1)))
+    numbers.append(number)
+
+print('The first number is {}'.format(numbers[0]))
+print('The last number is {}'.format(numbers[-1]))
+print('The smallest number is {}'.format(min(numbers)))
+print('The largest number is {}'.format(max(numbers)))
+print('The average of the numbers is {}'.format(sum(numbers)/len(numbers)))1"""
+
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input('Please enter your username: ')
+
+if username in usernames:
+    print('Access granted!!!')
+else:
+    print('Access denied!!!')
